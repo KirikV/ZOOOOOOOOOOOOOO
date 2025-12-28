efb776dff2fdb4958887a5f61a4586a2d238b67f
anopr <anoprienkok@gmail.com>
2025-12-28 15:38:26 +0300

    Корректировка параметров

--- src/Ilya.kt
+++ src/Ilya.kt
@@ -5,10 +5,10 @@ class Ilya(
     maxAge: Int,
     id: Int = 0
 ) : Petuh(
-    currentEnergy = 10,
+    currentEnergy = 15,
     currentAge = 1,
     currentWeight = 10,
-    maxAge = 5,
+    maxAge = 10,
     name = if (id == 0) "\uD83D\uDC13Илюха" else "\uD83D\uDC13Илюха №$id"
 ) {
     override val isVeryFat: Boolean get() = currentWeight >= currentEnergy
@@ -20,11 +20,25 @@ class Ilya(
 
     override fun move() {
         println("$name летает\uD83D\uDE80")
-        currentEnergy -= 5
-        currentWeight -= 10
+        currentEnergy -= 30
+        currentWeight -= 2
         if (isLowEnergy) {
             println("$name помер от недомогания\uD83E\uDD40")
             status = false
         }
+    }
+    override fun eat() {
+        println("$name очень мало ест\uD83D\uDE0B")
+        currentEnergy += 2
+        currentWeight += 1
+        if (isVeryFat) {
+            println("$name лопнул от переедания\uD83D\uDCA5")
+            status = false
+        }
+    }
+
+    override fun sleep() {
+        println("$name очень мало спит. Думает об Алиночке\uD83D\uDE35\u200D\uD83D\uDCAB")
+        currentEnergy += 5
     }
 }
